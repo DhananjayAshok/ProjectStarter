d997ab70b3fa507d20a7c1e24c421a494c425dc5
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-03-13 12:08:26 -0700

    none

--- scripts/get_strings.py
+++ scripts/get_strings.py
@@ -92,9 +92,10 @@ def parse():
             raise ValueError(f"Duplicate argument key: {args[i][2:]}")
         if args[i+1].startswith("--"):
             raise ValueError(f"consecutive --s: {args}")
-        if args[i+1].strip().lower() == "none":
-            continue
-        arg_dict[args[i].strip("--").lower()] = args[i+1]
+        if args[i + 1].strip().lower() == "none":
+            args[i + 1] = None
+        else:
+            arg_dict[args[i].strip("--").lower()] = args[i + 1]
     return string_name, arg_dict
 
 
